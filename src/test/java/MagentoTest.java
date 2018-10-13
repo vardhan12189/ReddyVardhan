46f6b11d594eb327461813cbe95307100feaff31
Geek <Geek@HariPrasad>
2018-10-13 12:39:41 +0530

    reddy2

--- src/test/java/MagentoTest.java
+++ src/test/java/MagentoTest.java
@@ -3,14 +3,14 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 public class MagentoTest 
 {
 	@Test
 	public void posCredentials() throws Exception {
 		String url="http://www.magento.com";
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
